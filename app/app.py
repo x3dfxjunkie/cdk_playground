a937744d481dc151e8b1a52f15c09e5cf321e09e
Russell Anderson <russell.anderson@disney.com>
2023-05-23 13:10:23 -0400

    test action

--- app/app.py
+++ app/app.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
-import aws_cdk as cdk
+from aws_cdk import App, Environment
+from app.app_stack import CdkWorkshopStack
 
-from app.app_stack import AppStack
+app = App()
 
+environment_type = app.node.try_get_context("environmentType")
+environment_context = app.node.try_get_context(environment_type)
+region = environment_context["region"]
+account = app.node.try_get_context("account")
+tags = environment_context["tags"]
+stack_name = f'{app.node.try_get_context("prefix")}-{environment_type}'
 
-app = cdk.App()
-AppStack(app, "app")
+CdkWorkshopStack(
+    app,
+    stack_name,
+     env = Environment(
+        account = account,
+        region = region
+    ),
+    tags=tags,
+)
 
 app.synth()
--- app/app/app_stack.py
+++ app/app/app_stack.py
@@ -1,24 +1,67 @@
+from datetime import datetime
+from aws_cdk import Stack, RemovalPolicy
 from constructs import Construct
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
+from aws_cdk.aws_lambda import Function, Runtime, Code, Alias, VersionOptions
+from aws_cdk.aws_apigateway import LambdaRestApi, StageOptions
+from aws_cdk.aws_cloudwatch import Alarm, ComparisonOperator
+from aws_cdk.aws_codedeploy import LambdaDeploymentGroup, LambdaDeploymentConfig
 
+class CdkWorkshopStack(Stack):
 
-class AppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self,
-            "AppQueue",
-            visibility_timeout=Duration.seconds(300),
+        environment_type = self.node.try_get_context("environmentType")
+        context = self.node.try_get_context(environment_type)
+        self.alias_name = context["lambda"]["alias"]
+        self.stage_name = context["lambda"]["stage"]
+        current_date =  datetime.today().strftime('%d-%m-%Y')
+
+        my_lambda = Function(
+            scope = self,
+            id = "MyFunction",
+            function_name= context["lambda"]["name"],
+            handler = "handler.lambda_handler",
+            runtime = Runtime.PYTHON_3_9,
+            code = Code.from_asset("lambda"),
+            current_version_options = VersionOptions(
+                description = f'Version deployed on {current_date}',
+                removal_policy = RemovalPolicy.RETAIN
+            )
         )
 
-        topic = sns.Topic(self, "AppTopic")
+        new_version = my_lambda.current_version
+        new_version.apply_removal_policy(RemovalPolicy.RETAIN)
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        alias = Alias(
+            scope = self,
+            id = "FunctionAlias",
+            alias_name = self.alias_name,
+            version = new_version
+        )
+
+        LambdaRestApi(
+            scope = self,
+            id = "RestAPI",
+            handler = alias,
+            deploy_options = StageOptions(stage_name=self.stage_name)
+        )
+
+        failure_alarm = Alarm(
+            scope = self,
+            id = "FunctionFailureAlarm",
+            metric = alias.metric_errors(),
+            threshold = 1,
+            evaluation_periods = 1,
+            alarm_description = "The latest deployment errors > 0",
+            alarm_name = f'{self.stack_name}-canary-alarm',
+            comparison_operator = ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        )
+
+        LambdaDeploymentGroup(
+            scope = self,
+            id = "CanaryDeployment",
+            alias = alias,
+            deployment_config = LambdaDeploymentConfig.CANARY_10_PERCENT_5_MINUTES,
+            alarms = [failure_alarm]
+        )
